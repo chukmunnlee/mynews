c135530669ef5c78df96dace3c270a8523ae37d8
Chuk <chukmunnlee@gmail.com>
2019-11-19 14:59:10 +0800

    travis.yml

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ if (fs.existsSync(DB_CONFIG)) {
 } else {
 	dbConfig = {
 		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
+		port: process.env.DB_PORT || 3306,
 		user: process.env.DB_USER,
 		password: process.env.DB_PASSWORD,
 		database: 'mynews',
